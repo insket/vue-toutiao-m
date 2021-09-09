bdb7b14247f8cef759961fc8537b5f7b7e2fa2b6
insket <2694183181@qq.com>
2021-09-09 14:52:31 +0800

    用户页完成并与注册登录实现交互，mock了用户user的数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -11,7 +11,7 @@ const request = axios.create({
 
 //请求拦截器
 request.interceptors.request.use((config) => {
-  //利用请求拦截器统一设置token
+  //  利用请求拦截器统一设置token
   const {user} = store.state
   if (user) {
     config.headers.Authorization = `Bearer ${user.token}`
